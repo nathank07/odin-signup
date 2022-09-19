fde6cb338ffbecec96a1f82de881816089a9d452
nathank7256 <nathank7256@gmail.com>
2022-09-18 19:15:14 -0500

    Added form, box shadows, password check, and adjust sizes

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("I've got swag")
+const password = document.querySelectorAll('.password');
+const passwordLabel = document.querySelector('.error');
+const button = document.querySelector('button');
+
+button.addEventListener('click', (e) => {
+    if(password[0].value != password[1].value){
+        password.forEach(box => {
+            box.style.borderColor = "red";
+        });
+        passwordLabel.style.opacity = 1;
+    }
+    else{
+        password.forEach(box => {
+            box.style.borderColor = "#b2b3b4";
+        });
+        passwordLabel.style.opacity = 0;
+    }
+});
+
